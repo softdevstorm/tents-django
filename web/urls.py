eed696bec24a003b5f31f1355c3142d4937d26e9
softdevstorm <softdevstorm@gmail.com>
2019-07-13 10:47:13 -0400

    update

--- web/urls.py
+++ web/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.index, name='index'),
-	path('gallery/<int:gallery_id>/', views.gallery, name='gallery')
+    path('', views.index, name='index'),
+    path('gallery/<int:gallery_id>/', views.gallery, name='gallery')
 ]
--- web/views.py
+++ web/views.py
@@ -3,21 +3,21 @@ from .models import Photo, Gallery
 
 
 def index(request):
-	gallery_list = Gallery.objects.all()
+    gallery_list = Gallery.objects.all()
 
-	context = {
-		'gallery_list': gallery_list
-	}
+    context = {
+        'gallery_list': gallery_list
+    }
 
-	return render(request, 'web/index.html', context=context)
+    return render(request, 'web/index.html', context=context)
 
 
 def gallery(request, gallery_id):
-	gallery = Gallery.objects.get(pk=gallery_id)
-	photos = gallery.photo_set.all()
+    gallery = Gallery.objects.get(pk=gallery_id)
+    photos = gallery.photo_set.all()
 
-	context = {
-		'photos': photos
-	}
+    context = {
+        'photos': photos
+    }
 
-	return render(request, 'web/gallery_content.html', context=context)
+    return render(request, 'web/gallery_content.html', context=context)
