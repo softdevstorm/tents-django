094260b53d72c468d13d6c4f99ba07927239f890
softdevstorm <softdevstorm@gmail.com>
2019-07-15 09:54:22 -0400

    update

--- web/urls.py
+++ web/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('gallery/<int:gallery_id>/', views.gallery, name='gallery'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('gallery/<int:gallery_id>/', views.gallery_detail, name='gallery-detail'),
     path('change-photo', views.change_photo, name='change_photo'),
     path('delete-photo', views.delete_photo, name='delete_photo'),
     path('add-new-photo', views.add_new_photo, name='add_new_photo'),
--- web/views.py
+++ web/views.py
@@ -1,6 +1,6 @@
 import os
 
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response, redirect
 from django.http import HttpResponse
 from .models import Photo, Gallery
 
@@ -15,7 +15,11 @@ def index(request):
     return render(request, 'web/index.html', context=context)
 
 
-def gallery(request, gallery_id):
+def gallery(request):
+    render(request, 'web/gallery_content.html')
+
+
+def gallery_detail(request, gallery_id):
     gallery = Gallery.objects.get(pk=gallery_id)
     photos = gallery.photo_set.all()
 
@@ -55,11 +59,11 @@ def delete_photo(request):
 def add_new_photo(request):
     if request.method == 'POST':
         gallery_id = int(request.POST.get('gallery_id'))
-        gallery = Gallery.objects.filter(id=gallery_id)
-        image = request.FILES.get('image')
-        name = request.POST.get('name')
+        gallery = Gallery.objects.filter(id=gallery_id).first()
         photo = Photo.objects.create(
             name=request.POST.get('name'),
             photo=request.FILES.get('image'),
             gallery=gallery
         )
+
+        return HttpResponse({'success': 'success'}, status=200)
