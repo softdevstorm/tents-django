2ace6ce0dc718552e4b62060d14631a6e0e712ea
softdevstorm <softdevstorm@gmail.com>
2019-07-18 23:20:25 -0400

    update

--- tents/settings.py
+++ tents/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(l$66(7vy#2uoes7t85-1x_70h@&hh%osocl3^65v7nkd=g+uj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '18.217.140.142', 'localhost', '127.0.0.1', 'tents.ge']
 
 
 # Application definition
